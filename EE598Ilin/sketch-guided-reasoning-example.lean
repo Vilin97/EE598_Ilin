17d244cef1308985e35daa467fcb04d18104ef3c
Vasily Ilin <vasilin97@gmail.com>
2026-02-02 13:40:57 -0800

    .

--- EE598Ilin/sketch-guided-reasoning-example.lean
+++ EE598Ilin/sketch-guided-reasoning-example.lean
@@ -41,7 +41,7 @@ theorem IsNoetherianRing.acc' (R : Type*) [CommRing R] [IsNoetherianRing R]
   have h2 : chain m <= chain hk := sorry -- maximality of chain hk in S
   exact le_antisymm h1 h2
 
-theorem IsNoetherianRing.acc [CommRing R] [IsNoetherianRing R]
+theorem IsNoetherianRing.acc'' [CommRing R] [IsNoetherianRing R]
     (chain : OrderHom Nat (Ideal R)) :
     Exists fun n => forall m, n <= m -> chain n = chain m := by
   set S := Set.range chain
@@ -58,3 +58,27 @@ theorem IsNoetherianRing.acc [CommRing R] [IsNoetherianRing R]
   have h1 : chain hk <= chain m := sorry -- ascending chain
   have h2 : chain m <= chain hk := sorry -- maximality of chain hk in S
   exact le_antisymm h1 h2
+
+/-- A noetherian ring (where by definition every ideal is finitely
+generated) satisfies the ascending chain condition
+-/
+theorem IsNoetherianRing.acc [CommRing R] [IsNoetherianRing R]
+    (I : ℕ → Ideal R) (h_chain : ∀ k, I k ≤ I (k + 1)) :
+   ∃ n, ∀ m ≥ n, I n = I   m := by
+  -- let J be the union of the ideals I n
+  let J : Ideal R := ⨆ n, I n
+  -- Since R is noetherian, J is finitely generated
+  have J_fg : J.FG := IsNoetherian.noetherian J
+  -- Let G be a finite set of generators of J
+  obtain ⟨G : Finset R, hs : Ideal.span (↑G) = J⟩ := J_fg
+  -- Choose n such that each generator g ∈ G is in Iₙ
+  have h : ∃ n, ∀ g ∈ G, g ∈ I n := by
+    sorry
+    -- << for each g ∈ G, ∃ n_g, g ∈ I n_g.  Take n = max {n_g | g ∈ G} >>
+  obtain ⟨n, hn⟩ := h
+  -- Let's show that the n obtained from h works
+  have h2 :  ∀ m ≥ n, I n = I m := by
+    sorry
+    -- << since every generator of J is in I n, J = I n.
+    -- for each m ≥ n, since I n ≤ I m ≤ J, we have I n = I m >>
+  use n
